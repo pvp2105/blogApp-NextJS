01f263cec313acb9b788a52d72183604492e9dea
pvp2105 <phuanh2105@gmail.com>
2023-07-05 15:43:16 +0700

    add SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,21 @@ import styles from "../styles/Home.module.css";
 import { MainLayout } from "@/components/layout";
 import { NextPageWithLayout } from "./_app";
 import { FeatureWorks, HeroSection, RecentPosts } from "@/components/home";
+import { Seo } from "@/components/common";
 
 const Home: NextPageWithLayout = () => {
   return (
     <Box>
+      <Seo
+        data={{
+          title: "NextJS Tutorials | Van Phu",
+          description:
+            "Step by step tutorials to build a full CRUD website using NextJS for beginners",
+          url: "https://learn-nextjs-fawn.vercel.app/",
+          thumbnailUrl:
+            "https://cdn.getshifter.co/caa65008efb706a8bfc6f7e4045d6a018420c3df/uploads/2020/11/nextjs.png",
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FeatureWorks />
